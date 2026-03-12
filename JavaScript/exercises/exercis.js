6a91bc7e341e25d571fb941f1a218661bdd87f05
Waqas Maqbool <maqbool.webdev@gmail.com>
2026-03-12 16:30:15 +0500

    Solved sumOfTripledEvens() with map()filter()reduce()

--- JavaScript/exercises/exercis.js
+++ JavaScript/exercises/exercis.js
@@ -149,3 +149,14 @@ function largeNumber(arr) {
 }
 
 console.log(largeNumber(numbers));
+
+let array = [3, 63, 3, 88, 33, 1, 9, 36, 4];
+function sumOfTripledEvens(array) {
+  return array
+  .filter((num) => num % 2 === 0)
+  .map((num) => num * 3)
+  .reduce((acc, curr) => acc + curr);
+
+}
+
+console.log(sumOfTripledEvens(array));
