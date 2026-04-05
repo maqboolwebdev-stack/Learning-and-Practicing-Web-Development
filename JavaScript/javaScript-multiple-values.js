efba2c6931221879a05374a407d02de80896c5e5
waqas <maqbool.webdev@gmail.com>
2026-04-05 19:39:32 +0500

    Learned some about array

---
+++ JavaScript/javaScript-multiple-values.js
@@ -0,0 +1,20 @@
+// let's learn about Array's
+
+let arr1 = new Array('purple', 'green', 'yellow');
+let arr2 = ['black', 'white', 'pink'];
+
+let arr3 = new Array(10);
+let arr4 = [10];
+
+console.log(arr3);
+console.log(arr4);
+
+const arr = ['Hi there!', 5, true];
+arr[0] = 'New value';
+
+console.log(typeof arr[0]);
+console.log(typeof arr[1]);
+console.log(typeof arr[2]);
+
+console.log(arr[0]);
+arr = ['is it valid?']
