84afdd7720ad7640f791e6289610f4e3f8a403bd
waqas <maqbool.webdev@gmail.com>
2026-04-06 22:09:35 +0500

    Learning continue

--- JavaScript/javaScript-multiple-values.js
+++ JavaScript/javaScript-multiple-values.js
@@ -17,4 +17,28 @@ console.log(typeof arr[1]);
 console.log(typeof arr[2]);
 
 console.log(arr[0]);
-arr = ['is it valid?']
+// arr = ['is it valid?']
+
+arr2[3] = ['guava'];
+arr2[-1] = ['caret'];
+
+console.log(arr2[3]);
+console.log(arr2[-1]);
+
+let animals = ['dog', 'cat', 'cow', 'loin'];
+let cars = ['ferrari', 'mercedes'];
+let emptyArray = [];
+
+console.log('length of animals:',animals.length);
+console.log('length of cars:',cars.length);
+console.log('length of emptyArray:',emptyArray.length);
+
+// Exercise 3.1
+
+let shoppingList = ['Milk', 'Bread', 'Apples'];
+
+console.log(shoppingList.length);
+
+shoppingList[1] = 'Banana';
+
+console.log(shoppingList);
