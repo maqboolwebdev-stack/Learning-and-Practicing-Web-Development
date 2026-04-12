916bf00d92d67ab11e15ddb10afaf66f1eb55556
waqas <maqbool.webdev@gmail.com>
2026-04-12 08:20:46 +0500

    learn if else

---
+++ JavaScript/logic-statements.js
@@ -0,0 +1,33 @@
+let rain = true;
+if(rain) {
+    console.log('** Taking my Umbrella when I need to go outside**');
+} else {
+    console.log('** I can leave my Umbrella at home **');
+}
+
+let age = 10;
+
+if(age < 18) {
+    console.log(`we're very sorry, but you can't get in under 18`);
+} else {
+    console.log('Welcome');
+}
+
+let hobby = 'dancing';
+
+if(hobby == 'coding') {
+    console.log('** I love coding too! **');
+} else {
+    console.log('** can you teach me that? **');
+}
+
+// Practice Exercise 4.1
+
+let myVariable = true;
+
+if(myVariable) {
+    console.log('Nice condition is True');
+}else {
+    console.log('what happening?');
+}
+
