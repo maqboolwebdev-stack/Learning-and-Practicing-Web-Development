9109e25088760ee66380b7f84c64d4c34feca0b5
waqas <maqbool.webdev@gmail.com>
2026-05-23 10:36:16 +0500

    Solve no.3 problem

--- JavaScript-TOP/21-days-challange-js/day-02/conditional-logic.js
+++ JavaScript-TOP/21-days-challange-js/day-02/conditional-logic.js
@@ -36,3 +36,24 @@ function checkNumberStatus(a) {
 checkNumberStatus(3);
 checkNumberStatus(-3.9);
 checkNumberStatus(0.1);
+
+// Q3 Calculate Electricity Bill
+
+function calculateElectricityBill(units) {
+    let bill;
+    if(units <= 100) {
+        bill = units * 5;
+    } else if(units <= 200) {
+        bill = (100 * 5) + (units - 100) * 7;
+    } else if(units <= 300) {
+        bill = (100 * 5) + (100 * 7) + (units - 200) * 10;
+    } else if(units >= 300) {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + (units - 300) * 12;
+    } else {
+        console.log('Invalid Units');
+        return;
+    }
+    console.log(`Total Bill: PKR-${bill}`);
+};
+
+calculateElectricityBill(230);
